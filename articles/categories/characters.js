70bbb81010507951acd78cc6362b2a47c7acdd15
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2025-05-29 09:45:46 +1000

    Assigning elements

--- articles/categories/characters.js
+++ articles/categories/characters.js
@@ -24,16 +24,33 @@ async function createProfile(element) {
     item.getElementsByClassName("cubeIcon")[0].src = "/images/hybrids/" + details["icon"] + ".png";
 
     let monsterElement = item.getElementsByClassName("monsterElementList")[0];
-    for (i = 0; i < details["elements"].length; i++) {
-        let elementElement = document.createElement("img");
-        let elementAttribute = document.createAttribute("src");
-        elementAttribute.value = "/images/elements/" + details["elements"][i] + ".png";
-        elementElement.setAttributeNode(elementAttribute)
-        let elementClass = document.createAttribute("class");
-        elementClass.value = "monsterElement";
-        elementElement.setAttributeNode(elementClass)
-        monsterElement.appendChild(elementElement)
-    }
+
+    if (details["elements"] == "inconclusive") {
+        /*TODO - Have a function loop every frame when triggered to rapidly blip between Filler's elements.*/
+
+        /*let elementScript = document.createElement("script");
+
+        let elementSource = document.createAttribute("src");
+        elementSource.value = "/articles/categories/inconclusiveElement.js"
+        elementScript.setAttributeNode(elementSource)
+
+        let elementType = document.createAttribute("type");
+        elementType.value = "text/javascript"
+        elementScript.setAttributeNode(elementType)
+
+        monsterElement.appendChild(elementScript)*/
+    } else {
+        for (i = 0; i < details["elements"].length; i++) {
+            let elementElement = document.createElement("img");
+            let elementAttribute = document.createAttribute("src");
+            elementAttribute.value = "/images/elements/" + details["elements"][i] + ".png";
+            elementElement.setAttributeNode(elementAttribute)
+            let elementClass = document.createAttribute("class");
+            elementClass.value = "monsterElement";
+            elementElement.setAttributeNode(elementClass)
+            monsterElement.appendChild(elementElement)
+        }
+    };
 
     element.appendChild(item);
 }
---
+++ articles/categories/inconclusiveElement.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=>{ /*How to trigger code.*/
+    inconclusiveElement()
+})
+
+function inconclusiveElement() {
+    console.log("Hello world")
+}
