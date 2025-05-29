d2262b093259be296766a91ddf80a306ea8b22ea
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2025-05-29 12:00:30 +1000

    Filler stuff

--- articles/categories/characters.js
+++ articles/categories/characters.js
@@ -1,12 +1,48 @@
+let inconclusiveElementDisplay = [];
+let elementImages = [];
+
 document.addEventListener('DOMContentLoaded', ()=>{ /*How to trigger code.*/
+    initiateInconclusiveElement()
     generateProfiles()
+    
 })
+
 
 function generateProfiles() {
     let cubePages = document.body.getElementsByClassName("prev");
 
     for (i = 0; i < cubePages.length; i++) {
         createProfile(cubePages[i])
+    }
+}
+
+async function initiateInconclusiveElement() {
+    setInterval(inconclusiveElement, 100)
+
+    const response = await fetch("/images/elements");
+    let file = await response.text();
+    let parser = new DOMParser();
+    let imagesFile = parser.parseFromString(file, "text/html");
+
+    elementImages = imagesFile.getElementsByTagName("a");
+    console.log(elementImages)
+}
+function inconclusiveElement() {
+    for (p = 0; p < inconclusiveElementDisplay.length; p++) {
+        let elementDisplay = inconclusiveElementDisplay[p];
+
+        let IEcount = Math.round(Math.random() * 6 + 1);
+        elementDisplay.innerHTML = "";
+
+        for (i = 0; i < IEcount; i++) {
+            /* let imgPointer = Math.round(Math.random() * (elementImages.length - 1));
+
+            let elementElement = document.createElement("img");
+            let elementAttribute = document.createAttribute("src");
+            elementAttribute.value = elementImages[imgPointer].href;
+            elementElement.setAttributeNode(elementAttribute)
+            elementDisplay.appendChild(elementElement) */
+        }   
     }
 }
 
@@ -26,28 +62,15 @@ async function createProfile(element) {
     let monsterElement = item.getElementsByClassName("monsterElementList")[0];
 
     if (details["elements"] == "inconclusive") {
+        
         /*TODO - Have a function loop every frame when triggered to rapidly blip between Filler's elements.*/
-
-        /*let elementScript = document.createElement("script");
-
-        let elementSource = document.createAttribute("src");
-        elementSource.value = "/articles/categories/inconclusiveElement.js"
-        elementScript.setAttributeNode(elementSource)
-
-        let elementType = document.createAttribute("type");
-        elementType.value = "text/javascript"
-        elementScript.setAttributeNode(elementType)
-
-        monsterElement.appendChild(elementScript)*/
+        inconclusiveElementDisplay.push(monsterElement)
     } else {
         for (i = 0; i < details["elements"].length; i++) {
             let elementElement = document.createElement("img");
             let elementAttribute = document.createAttribute("src");
             elementAttribute.value = "/images/elements/" + details["elements"][i] + ".png";
             elementElement.setAttributeNode(elementAttribute)
-            let elementClass = document.createAttribute("class");
-            elementClass.value = "monsterElement";
-            elementElement.setAttributeNode(elementClass)
             monsterElement.appendChild(elementElement)
         }
     };
